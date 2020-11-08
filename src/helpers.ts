3c852ed90815d0c21ef8c61055035b06fdbf7349
hulya <hulyakarakayaa06@gmail.com>
2020-11-08 15:32:36 -0800

    add type checking

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,12 +1,14 @@
 // Convert time to hours and minutes
-export const calcTime = time => {
-  const hours = Math.floor(time / 60);
-  const mins = time % 60;
+export const calcTime = (time : number ) : string => {
+  const hours : number = Math.floor(time / 60);
+  const mins  : number = time % 60;
   return `${hours}h ${mins}m`;
 };
+
 // Convert a number to money formatting
-export const convertMoney = money => {
-  const formatter = new Intl.NumberFormat('en-US', {
+export const convertMoney = (money  : number ) : string=> {
+  // international number format
+  const formatter : Intl.NumberFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 0
@@ -14,7 +16,8 @@ export const convertMoney = money => {
   return formatter.format(money);
 };
 
-export const isPersistedState = stateName => {
+// JSON.parse can have any type
+export const isPersistedState = (stateName : string) : any => {
   const sessionState = sessionStorage.getItem(stateName);
-  return sessionStorage && JSON.parse(sessionState);
+  return sessionState&& JSON.parse(sessionState);
 };
